45a5e4d28a3c68621b79d30866e42e97163db0a8
HuyHoangEngr <alegiahuy11@gmail.com>
2025-03-01 22:46:46 +0700

    video23.Tao Tat Ca Models, Migrations va Seeders cho du an dat lich kham benh - Phan 1

---
+++ src/migrations/migration-create-booking.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    //   statusId: DataTypes.STRING,
+    //   doctorId: DataTypes.INTEGER,
+    //   patientID: DataTypes.INTEGER,
+    //   date: DataTypes.DATE,
+    //   timeType: DataTypes.STRING,
+    await queryInterface.createTable("bookings", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      doctorId: {
+        type: Sequelize.INTEGER,
+      },
+      patientID: {
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      timeType: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("bookings");
+  },
+};
---
+++ src/migrations/migration-create-clinic.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    //   statusId: DataTypes.STRING,
+    //   doctorId: DataTypes.INTEGER,
+    //   patientID: DataTypes.INTEGER,
+    //   date: DataTypes.DATE,
+    //   timeType: DataTypes.STRING,
+    await queryInterface.createTable("clinics", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      address: {
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("clinics");
+  },
+};

d6e6fde531db1c808a2d3308740c9a57ba565684
HuyHoangEngr <alegiahuy11@gmail.com>
2025-03-12 13:33:31 +0700

    video35.Viet APIs chuanMEN voi Node.js | Cach viet REST APIs cho nguoi moi bat dau

--- src/route/web.js
+++ src/route/web.js
@@ -1,18 +1,21 @@
 import express from "express";
 import homeController from "../controllers/homeController";
-
+import userController from "../controllers/userController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", homeController.getHomePage);
-  router.get("/about", homeController.getAboutPage);
-  router.get("/crud", homeController.getCRUD);
-  router.post("/post-crud", homeController.postCRUD);
-  router.get("/get-crud", homeController.displayGetCRUD);
-  router.get("/edit-crud", homeController.getEditCRUD);
-  router.post("/put-crud", homeController.putCRUD); //phuong thuc .put chua thuc hien duoc
-  router.get("/delete-crud", homeController.deleteCRUD); //the a cho nut delete mac dinh la phuong thuc get
-  return app.use("/", router);
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
+    router.get("/crud", homeController.getCRUD);
+    router.post("/post-crud", homeController.postCRUD);
+    router.get("/get-crud", homeController.displayGetCRUD);
+    router.get("/edit-crud", homeController.getEditCRUD);
+    router.post("/put-crud", homeController.putCRUD); //phuong thuc .put chua thuc hien duoc
+    router.get("/delete-crud", homeController.deleteCRUD); //the a cho nut delete mac dinh la phuong thuc get
+
+    router.post("/api/login", userController.handleLogin);
+
+    return app.use("/", router);
 };
 
 module.exports = initWebRoutes;

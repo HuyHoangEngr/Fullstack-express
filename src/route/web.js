4e9ab1bc7bb612f10dbb1ae85ac52f7d0d7c75d7
HuyHoangEngr <alegiahuy11@gmail.com>
2025-03-14 22:04:57 +0700

    video37. CRUD voi ReactJS phan 1 - Viet API lay tat ca thong tin nguoi dung

--- src/route/web.js
+++ src/route/web.js
@@ -14,6 +14,7 @@ let initWebRoutes = (app) => {
     router.get("/delete-crud", homeController.deleteCRUD); //the a cho nut delete mac dinh la phuong thuc get
 
     router.post("/api/login", userController.handleLogin);
+    router.get("/api/get-all-users", userController.handleGetAllUsers);
 
     return app.use("/", router);
 };
